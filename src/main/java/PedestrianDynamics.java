2bcdd65eccf3f9109f33f5860f06ce891b4bedd8
micaelaperillo <micaelaperillo26@gmail.com>
2025-06-02 11:51:15 -0300

    stats: graphs for c

--- src/main/java/PedestrianDynamics.java
+++ src/main/java/PedestrianDynamics.java
@@ -7,25 +7,32 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public static void main(String[] args) throws Exception {
-    int qIn = args.length > 0 ? Integer.parseInt(args[0]) : 1;
-    int simulationTime = args.length > 1 ? Integer.parseInt(args[1]) : 120;//segundos
 
-    Parameters p = Parameters.builder()
-            .inflow(qIn)
-            .build();
+    // Run same qIn range i times
+    for (int i = 1; i < 10; i++) {
+        // Run many values for qIn
+        for (int qIn = 1; qIn < 50; qIn += 5) {
+            int simulationTime = args.length > 1 ? Integer.parseInt(args[1]) : 120;//segundos
 
-    SimulationEngine engine = new SimulationEngine(p, 20_000);
+            Parameters p = Parameters.builder()
+                    .inflow(qIn)
+                    .build();
 
-    Files.createDirectories(Paths.get("output"));
-    double time=0;
-    long tick=0;
-    String file_name = STR."output/run_\{qIn}.csv";
-    try (CsvFrameWriter writer = new CsvFrameWriter(file_name, p.dt(), p.outputDt())) {
-        while(!engine.isFinished()){
-            SimulationState state = engine.step(tick,time);
-            writer.writeFrameIfDue(state);
-            time+=p.dt();
-            tick++;
+            SimulationEngine engine = new SimulationEngine(p, 20_000);
+
+            Files.createDirectories(Paths.get("output"));
+            double time=0;
+            long tick=0;
+            String file_name = STR."output/run_\{qIn}_\{i}.csv";
+            try (CsvFrameWriter writer = new CsvFrameWriter(file_name, p.dt(), p.outputDt())) {
+                while(!engine.isFinished()){
+                    SimulationState state = engine.step(tick,time);
+                    writer.writeFrameIfDue(state);
+                    time+=p.dt();
+                    tick++;
+                }
+            }
         }
     }
+
 }
--- visualization/graphs.py
+++ visualization/graphs.py
@@ -12,13 +12,6 @@ if not os.path.exists(graphs_folder):
     os.makedirs(graphs_folder)
 
 def parse_output_file(file):
-    # data = {}
-    # with open(file, 'r') as f:
-    #     reader = csv.DictReader(f)
-    #     for row in reader:
-    #         time_key = float(row["time"]) 
-    #         value = {key: float(val) for key, val in row.items() if key != "time"}
-    #         data[time_key] = value
     data = pd.read_csv(file, index_col='time')
     return data
 
@@ -28,19 +21,27 @@ def parse_output_files(output_folder):
     files = os.listdir(output_folder)
     for file in files:
         if file.endswith('.csv'):
-            qin = int(file.split('_')[1].split('.')[0])
-            data[qin] = parse_output_file(os.path.join(output_folder, file))
-
+            qin, iteration = file.split('_')[1], file.split('_')[2].split('.')[0]
+            data[f"{qin}_{iteration}"] = parse_output_file(os.path.join(output_folder, file))
     return data
 
 
 ### Ex: b
 def plot_avg_vx_over_qin(data):
     results = {}
-    for qin in data.keys():
-        df = data[qin]
+    # Uses only the first iteration for each qin
+
+    for d in data.keys():
+        qin, iteration = d.split('_')
+
+        if iteration  != "1": 
+            continue
+
+        df = data[d]
         df = df.copy()
         df['second'] = df.index.astype(float).astype(int)
+
+        df = df[(df['second'] >= 10) & (df['second'] <= 40)] # 10s to 40s (or max available)
 
         grouped = df.groupby('second')
 
@@ -70,8 +71,47 @@ def plot_avg_vx_over_qin(data):
     plt.show()
 
     plt.savefig('graphs/avg_vx_over_qin.png')
+
+### Ex: c
+def plot_avg_t_vs_qin(data):
+
+    results = {}
+
+    for d in data.keys():
+        qin, iteration = d.split('_')
+        df = data[d]
+
+        max_time = df.index.max()
+
+        if qin not in results:
+            results[qin] = []
+        results[qin].append(max_time)
+
+    results_avg = {qin: np.mean(times) for qin, times in results.items()}
+    results_df = pd.DataFrame.from_dict(results_avg, orient='index', columns=['avg_max_time'])
+    results_df.index.name = 'qin'
+
+    sorted_qin = sorted(results_df.index, key=int)
+    avg_time_values = [results_df.loc[qin, 'avg_max_time'] for qin in sorted_qin]
+
+    std_error_time = results_df['avg_max_time'].std() / np.sqrt(len(results_df))
+    std_error_time_values = [std_error_time] * len(sorted_qin)
+
+    plt.errorbar(sorted_qin, avg_time_values, yerr=std_error_time_values, fmt='o', capsize=5)
+    plt.xlabel('Qin')
+    plt.ylabel('Tiempo Máximo Promedio')
+    plt.tight_layout()
+    plt.grid(True)
+    plt.show()
+
+    plt.savefig('graphs/avg_t_vs_qin.png')
+
+
+
+
+        
     
 
 data = parse_output_files('output')
-print(plot_avg_vx_over_qin(data))
-
+plot_avg_vx_over_qin(data)
+plot_avg_t_vs_qin(data)
